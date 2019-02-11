a923b9a850902942aeffea0394b195b0be20d660
Nikolay Neupokoev <neupokoev.n@gmail.com>
2019-02-10 18:10:22 -0800

    Highlight the main word in excerpt

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -2,30 +2,25 @@ import React, { Component } from 'react';
 import { Alert, Badge, Row, Col, Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
 
 class SearchResults extends Component {
-  constructor(props) {
-    super(props);
-  }
 
   renderResults() {
     var results = this.props.getResults()
-    console.log("renderResults", results)
     if (results.length === 0) {
       return <h2>Your search did not match any documents</h2>
     }
     return results.map(record => {
-      console.log("renderResults", record)
+      var title = (
+        <h2>
+          {record.title.excerpt1}<strong>{record.title.highlightWord}</strong>{record.title.excerpt2}{' '}<Badge color="secondary">{record.totalScore}</Badge>
+        </h2>
+      )
       return (
         <Card>
           <CardBody>
             <CardTitle>
-              <h2>
-                {record.title}{' '}<Badge color="secondary">{record.totalScore}</Badge>
-              </h2>
+              {title}
             </CardTitle>
-            <CardSubtitle>Description</CardSubtitle>
             <CardText>{record.breed}</CardText>
-            <CardSubtitle>Did you know?</CardSubtitle>
-            <CardText>{record.did_you_know}</CardText>
           </CardBody>
         </Card>
       )
--- src/engine.js
+++ src/engine.js
@@ -7,6 +7,7 @@ const bighugelabs = `//words.bighugelabs.com/api/2/${bighugelabs_api_key}/`
 
 let tagger = PosTagger();
 let sentenceTokenizer  = new Natural.SentenceTokenizer()
+let wordTokenizer = new Natural.WordTokenizer()
 let TermFrequency = Natural.TfIdf; // Term Frequency–Inverse Document Frequency
 let termFrequency = new TermFrequency();
 
@@ -38,7 +39,6 @@ class CatSearchEngine {
     var weightTasks = [];
     sentences.forEach(sentence => {
       var taggedSentence = tagger.tagSentence(sentence);
-      console.log(taggedSentence)
       taggedSentence.forEach(token => {
         var word = token.value;
         var pos = token.pos;
@@ -91,16 +91,13 @@ class CatSearchEngine {
       let t = tokens[i];
       words.push(t.word)
       words = words.concat(t.synonyms)
-      console.log("saveTokens", t.word, t.synonyms, words)
     }
-    console.log("saveTokens", tokens, words)
     Cats[breed].words = words;
     return tokens;
   }
 
   prepareBase() {
     for (const [breed, details] of Object.entries(Cats)) {
-      console.log(breed, details)
       let paragraph = details.size + ". " + details.coat + ". " + details.color + ". " + details.description + details.did_you_know
       this.weightTokens(paragraph)
       .then(weightedTokens => this.finalTokens(weightedTokens))
@@ -108,33 +105,88 @@ class CatSearchEngine {
     }
   }
 
-  scoreWords(a, b) {
-    return Natural.JaroWinklerDistance(a, b);
+  scoreWords(array, word) {
+    var score = 0;
+    for (let i = 0; i < array.length; ++i) {
+      score = 1/(Natural.LevenshteinDistance(array[i], word)+0.000001);
+    }
+    return score;
+    //return Natural.JaroWinklerDistance(a, b) > 0.5? 1 : 0;
+  }
+
+  highlightWord(sentence, word) {
+    var index = sentence.indexOf(word)
+    if (index !== -1) {
+      const neighborhoodSymbols = 30
+      var startPos = sentence.indexOf(" ", index - neighborhoodSymbols);
+      var endPos = sentence.indexOf(" ", index + word.length + neighborhoodSymbols);
+      var prefix = "", suffix = ""
+      if (startPos > index || startPos < 0) {
+        startPos = 0
+      } else {
+        prefix = "..."
+      }
+      if (endPos < index || endPos > sentence.length) {
+        endPos = undefined
+      } else {
+        suffix = "..."
+      }
+      var shortString = prefix + sentence.slice(startPos, endPos) + suffix;
+      var shortIndex = shortString.indexOf(word);
+      return {
+        index: shortIndex,
+        excerpt: shortString,
+        highlightWord: word,
+        excerpt1: shortString.slice(0, shortIndex),
+        excerpt2: shortString.slice(shortIndex + word.length)
+      }
+    } else {
+      return null
+    }
   }
 
   search(query) {
-    var result = []
+    var queryWords = wordTokenizer.tokenize(query);
+    var result = [];
     for (let breed in Cats) {
       let totalScore = 0;
-      let tokens = Cats[breed].tokens;
+      let data = Cats[breed]
+      let tokens = data.tokens;
+      let spotlight = {score: 0, word: ""};
       for (let t = 0; t < tokens.length; ++t) {
         let token = tokens[t];
-        let wordScore = 0
-        wordScore += this.scoreWords(query, token.word);
+        let wordScore = 0;
+        wordScore += this.scoreWords(queryWords, token.word);
         for (let s = 0; s < token.synonyms.length; ++s) {
-          let word = token.synonyms[s]
-          wordScore += this.scoreWords(query, word);
+          let word = token.synonyms[s];
+          wordScore += this.scoreWords(queryWords, word);
         }
         token.wordScore = wordScore;
         totalScore += wordScore;
+        if (wordScore > 0 && wordScore > spotlight.score) {
+          spotlight.score = wordScore;
+          spotlight.word = token.word;
+        }
       }
-      Cats[breed].totalScore = totalScore;
-      Cats[breed].breed = breed;
+      data.totalScore = totalScore;
+      data.breed = breed;
       if (totalScore > 0) {
-        result.push(Cats[breed]);
+        data.title =
+          this.highlightWord(data.did_you_know, spotlight.word) ||
+          this.highlightWord(data.description, spotlight.word) ||
+          this.highlightWord(data.size, spotlight.word) ||
+          this.highlightWord(data.coat, spotlight.word) ||
+          this.highlightWord(data.color, spotlight.word) ||
+          this.highlightWord(data.did_you_know, tokens[0].word) ||
+          this.highlightWord(data.description, tokens[0].word) ||
+          this.highlightWord(data.size, tokens[0].word) ||
+          this.highlightWord(data.coat, tokens[0].word) ||
+          this.highlightWord(data.color, tokens[0].word)
+        console.log('search', totalScore, spotlight, data.title, data)
+        result.push(data);
       }
     }
-    return result.sort((a, b) => a.score < b.score).splice(0, 5)
+    return result.sort((a, b) => a.totalScore < b.totalScore).splice(0, 5);
   }
 }
 
